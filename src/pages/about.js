4e24b36fc27bc7e9a5cc6c8b66fd7b987eb83496
David Currie <davidalexandercurrie@gmail.com>
2022-09-18 17:25:36 -0400

    line breaks antonia sam

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,14 +26,19 @@ const about = ({ location }) => {
         </p>
         <p className={pageP}>
           BLOT is co-edited by Antonia Barnett McIntosh and Samuel Holloway.
+        </p>
+        <p className={pageP}>
           Antonia! is a composer, performer, and sound artist. She collaborates
           in cross arts spaces, gently tapping on the borders between
           speech/music, performance/rehearsal, composition/writing, and
           juxtaposing the formalities of presentation with the aesthetics of
-          failure. Samuel: is a composer, performer, and educator. His creative
-          work and research is variously concerned with: music’s paratextual
-          language; ambiguous affective states, such as boredom; and the
-          continuing potential of musical notation as a site for exploration.
+          failure.
+        </p>
+        <p className={pageP}>
+          Samuel: is a composer, performer, and educator. His creative work and
+          research is variously concerned with: music’s paratextual language;
+          ambiguous affective states, such as boredom; and the continuing
+          potential of musical notation as a site for exploration.
         </p>
         <p className={pageP}>
           We are very grateful to the following for their generous support:
