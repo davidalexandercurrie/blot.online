3ef8f8d3e94469f3965feb152c76bcd6a2d80929
David Currie <davidalexandercurrie@gmail.com>
2022-09-19 08:49:28 -0400

    fix columns

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
-import '@fontsource/open-sans';
+import '@fontsource/work-sans';
 import {} from '../styles/Layout.module.css';
 
 const pageStyles = {
   margin: `0 auto`,
   padding: `0 0rem`,
-  fontFamily: `Open Sans`,
+  fontFamily: `Work Sans`,
 };
 
 export default function Layout({ children, headerText, root }) {
