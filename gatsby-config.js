3b67e1dc7c5c3321dc66477e921586232332e0a3
David Currie <davidalexandercurrie@gmail.com>
2022-09-20 01:43:41 -0400

    netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-smoothscroll`,
+    `gatsby-plugin-netlify`,
     {
       resolve: 'gatsby-source-contentful',
       options: {
