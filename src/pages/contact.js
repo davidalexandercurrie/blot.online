c72844fdecad0b9430b80d4b6732745f2c6f8f1a
David Currie <davidalexandercurrie@gmail.com>
2022-09-19 20:50:23 -0400

    mobile

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,28 +1,24 @@
 import * as React from 'react';
 import Layout from '../components/layout';
-import {
-  pageContainer,
-  pageTitle,
-  noBulletList,
-} from '../styles/Layout.module.css';
+import { pageContainer, pageTitle, pageP } from '../styles/Layout.module.css';
 
 const contact = () => {
   return (
     <Layout>
       <div className={pageContainer}>
         <h1 className={pageTitle}>Contact</h1>
-        <p className={noBulletList}>
+        <p className={pageP}>
           We welcome responses, proposals, criticism, support, all the feels.
         </p>
 
-        <p className={noBulletList}>
+        <p className={pageP}>
           Get in touch with us by email{' '}
           <a className="page-link" href="mailto:blotjournalonline@gmail.com">
             blotjournalonline@gmail.com
           </a>
         </p>
 
-        <p className={noBulletList}>
+        <p className={pageP}>
           Follow BLOT on instagram{' '}
           <a
             className="page-link"
